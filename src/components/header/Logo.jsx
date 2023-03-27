b8f0372b0190a7e2a5438807c9b3bbab2a7578c3
Florian Meiswinkel <florian.meiswinkel@outlook.de>
2023-03-27 10:57:06 +0200

    Bug fix Header

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -4,9 +4,10 @@ import Link from "next/link"
 
 const Logo = () => {
     return (
-        <Link href="/" className="flex flex-col items-start text-3xl space-x-2">
-            <Image src="/au-logo.png" alt="explorer logo" width={120} height={120} />
-            <h1 className="font-bold pl-4 text-slate-50">Explorer</h1>
+        <Link href="/" className="flex  items-end text-3xl ">
+            <h1 className="font-bold text-4xl text-blue-900 ">3</h1>
+
+            <h1 className="font-bold text-slate-50">xplorer</h1>
         </Link>
     )
 }
